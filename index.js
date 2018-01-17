506b57fcd286f7cdb2c0baef3739b23fe63a9d82
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-17 23:02:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const returnFirstTwoDrivers =function returnFirstTwoDrivers(drivers){
   let newDrivers = drivers.slice(0,2);
   return newDrivers;
 }
+
+const returnLastTwoDrivers =function returnLastTwoDrivers(drivers){
+  let newDrivers = drivers.slice(drivers.length-1,2);
+  return newDrivers;
+}

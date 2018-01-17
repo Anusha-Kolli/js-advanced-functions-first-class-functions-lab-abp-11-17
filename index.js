344a804c08945bee15b90d469e9f2ee1da00439b
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-17 22:54:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solution in this file!
-const firstVar = function returnFirstTwoDrivers(drivers){
+function returnFirstTwoDrivers(drivers){
   const newDrivers = drivers.slice(0,2);
   return newDrivers;
 }
-

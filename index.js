5869412c23dce9cb1fe29e8c0096a6c424ef7234
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-17 22:44:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
  function returnFirstTwoDrivers(drivers){
-  const newDrivers = drivers.slice(0,1);
+  const newDrivers = drivers.slice(0,2);
   return newDrivers;
 }
 
